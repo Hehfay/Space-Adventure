f771387193a82ee874550938a6c57adc4a919d18
Hehfay <jeffrey.hartman4691@gmail.com>
2014-09-24 14:19:30 -0400

    Moved the git ignore file to the correct spot ( i think )
    Will this work

---
+++ SpaceAdventure/Assets/Controller1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller1 : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+	}
+}
