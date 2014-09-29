e24f8a3b527da10ae1d186220b81926f29ef97ca
Hehfay <jeffrey.hartman4691@gmail.com>
2014-09-29 19:57:57 -0400

    Updated an enemy AI script.  It doesn't work yet.

--- SpaceAdventure/Assets/Scripts/Enemy_AI.cs
+++ SpaceAdventure/Assets/Scripts/Enemy_AI.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy_AI : MonoBehaviour {
+public class Enemy_AI : MonoBehaviour 
+{
+	private enum directions
+	{
+		left,
+		right
+	};
+
+	private directions currentDirection;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		currentDirection = directions.right;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+					switch ( currentDirection )
+					{
+									case directions.left:
+													transform.Translate(Vector3.left * 0.05f);
+													break;
+									case directions.right:
+													transform.Translate(Vector3.right * 0.05f);
+													break;
+					}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.tag == "Left")
+		{
+			currentDirection = directions.right;
+		}
+		if(other.gameObject.tag == "Right")
+		{
+			currentDirection = directions.left;
+		}
 	}
 }
