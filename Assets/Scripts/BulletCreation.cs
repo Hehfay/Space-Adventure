f20e6574242f2be8fe7f54d3dbec906d6618b8cc
TeejStroyer27 <titoriano@gmail.com>
2014-10-01 12:42:04 -0400

    a lot of nothing

--- Assets/Scripts/BulletCreation.cs
+++ Assets/Scripts/BulletCreation.cs
@@ -17,6 +17,14 @@ public class BulletCreation : MonoBehaviour {
 	void Shoot() {
 		
 		GameObject bullet = Instantiate(BulletPrefab) as GameObject;
-		bullet.rigidbody.AddForce(transform.forward * BulletSpeed);
+		//Instantiate(BulletPrefab) as GameObject;
+		
+		//bullet.rigidbody.AddForce(transform.forward * BulletSpeed);
+
+		Vector3 difference = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
+		difference.Normalize ();		// normalizing the vector. Meaning that all the sum of the vector will be equal to 1
+		
+		float rotZ = Mathf.Atan2 (difference.y, difference.x) * Mathf.Rad2Deg;	// find the angle in degrees
+		transform.rotation = Quaternion.Euler (0f, 0f, rotZ );
 	}
 }
