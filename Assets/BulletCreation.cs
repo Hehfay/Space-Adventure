68abc088f1e55b64f14f4e3d8b92b2d40e0a2247
TeejStroyer27 <titoriano@gmail.com>
2014-10-01 10:37:56 -0400

    Tinkering with bullet prefab

---
+++ Assets/BulletCreation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletCreation : MonoBehaviour {
+	
+	public GameObject BulletPrefab;
+	public float BulletSpeed;
+	
+	void Update () {
+		
+		if (Input.GetMouseButtonDown(0)){
+			
+			Shoot();
+		}
+	}
+	
+	void Shoot() {
+		
+		GameObject bullet = Instantiate(BulletPrefab) as GameObject;
+		bullet.rigidbody.AddForce(transform.forward * BulletSpeed);
+	}
+}
