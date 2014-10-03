dae8d06c5bc0a1a875861ef12ffa37197d50e9dc
Danny Thorne <dannythorne@gmail.com>
2014-10-03 17:12:13 -0400

    first sketch of scene change mechanism

---
+++ Assets/Scripts/DT_DemoDoor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DT_DemoDoor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Walk through door to get to a new scene.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D( Collider2D other)
+	{
+		Debug.Log("2D Trigger");
+		Application.LoadLevel(1);
+	}
+
+}
---
+++ Assets/Scripts/DT_TempPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DT_TempPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.x = pos.x + 0.1f;
+		transform.position = pos;
+	}
+
+}
