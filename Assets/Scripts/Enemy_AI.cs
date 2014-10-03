6395f47dd189c3cd8050625948d70f975054325b
Hehfay <jeffrey.hartman4691@gmail.com>
2014-10-03 01:14:32 -0400

    Added Ray Casting
    Adding Ray Casting to the enemy object.  As is it doesn't do anything
    interesting.  My thought is to find a way to make the enemy line of
    sight sweep rapidly over an arch length to simulate a vision cone.  Once
    that is done, I will define behaviors for various line of sight to tag
    collisions.

--- Assets/Scripts/Enemy_AI.cs
+++ Assets/Scripts/Enemy_AI.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Enemy_AI : MonoBehaviour 
 {
+	Vector2 sightDirection;
+
 	private enum directions
 	{
 		left,
@@ -12,9 +14,8 @@ public class Enemy_AI : MonoBehaviour
 
 	private enum alertStatus
 	{
-		idle,
-		caution,
-		alert
+		patroling,
+		attacking
 	};
 	private alertStatus enemyAlertStatus;
 
@@ -27,16 +28,22 @@ public class Enemy_AI : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-					switch ( currentDirection )
-					{
-									case directions.left:
-													transform.Translate(Vector3.left * 0.05f);
-													break;
-									case directions.right:
-													transform.Translate(Vector3.right * 0.05f);
-													break;
-					}
+		switch ( currentDirection ){
+			case directions.left:
+				Physics2D.Raycast (transform.position, sightDirection);
+				transform.Translate(Vector3.left * 0.05f);
+				break;
+			case directions.right:
+				transform.Translate(Vector3.right * 0.05f);
+				Physics2D.Raycast (transform.position, sightDirection);
+				break;
+		}
+	}
 
+	void OnDrawGizmos(){
+		Vector2 direction = new Vector2 (5, 0);
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine (transform.position, direction);
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
@@ -44,10 +51,13 @@ public class Enemy_AI : MonoBehaviour
 		if(other.gameObject.tag == "Left")
 		{
 			currentDirection = directions.right;
+			sightDirection = new Vector2(1,0);
 		}
 		if(other.gameObject.tag == "Right")
 		{
 			currentDirection = directions.left;
+			sightDirection = new Vector2(-1,0);
 		}
 	}
 }
+
