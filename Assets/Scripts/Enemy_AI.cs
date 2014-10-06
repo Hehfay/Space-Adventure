d22ff201cbe15bdfe144e47670aee7c246cb7a5e
Hehfay <jeffrey.hartman4691@gmail.com>
2014-10-06 17:26:51 -0400

    Updated the script
    Cleaned up, added different speeds.

--- Assets/Scripts/Enemy_AI.cs
+++ Assets/Scripts/Enemy_AI.cs
@@ -3,46 +3,63 @@ using System.Collections;
 
 public class Enemy_AI : MonoBehaviour 
 {
-	/* Enemy's current facing direction flag */
+	/*************/
+	/* VARIABLES */
+	/*************/
+
+	// Enemy Speeds
+	static private float standingSpeed   = 0.0f;
+	static private float patrollingSpeed = 0.025f;
+	static private float attackingSpeed  = 0.075f;
+	float[] speed = {standingSpeed, patrollingSpeed, attackingSpeed};
+
+	// Enemy's current direction flag 
 	private enum directions{
 		left,
 		right
-	};
+	}; 
 	private directions currentDirection;
 	
-	/* Indicating what state the enemy is in */
+	// Indicating what state the enemy is in 
 	private enum alertStatus{
 		standing,
-		patroling,
+		patrolling,
 		attacking
 	}; 
 	private alertStatus enemyAlertStatus;
 	
-	/* Used for ray cast hit detection */	
+	// Used for ray cast hit detection 
 	RaycastHit2D hit;
-		
-	/* Use this for initialization */
+
+	/*************/
+	/* Functions */
+	/*************/
+
+	// Use this for initialization 
 	void Start(){
-		currentDirection = directions.right;
+		currentDirection = directions.left;
+		enemyAlertStatus = alertStatus.attacking;
 	}
 
+	// Called on fixed intervals
 	void FixedUpdate(){	
 		switch ( currentDirection ){
 
 			case directions.left:
-				transform.Translate(Vector3.left * 0.05f);
+				transform.Translate(Vector3.left * speed[(int)enemyAlertStatus]);
 				hit = Physics2D.Raycast (transform.position, transform.TransformDirection(Vector3.left), 10);
 				rayCollision(hit);	
 				break;
 
 			case directions.right:
-				transform.Translate(Vector3.right * 0.05f);
+				transform.Translate(Vector3.right * speed[(int)enemyAlertStatus]);
 				hit = Physics2D.Raycast (transform.position, transform.TransformDirection(Vector3.right), 10);
 				rayCollision(hit);	
 				break;
 		}
 	}
-
+	
+	// Collisions between the enemy and other tags
 	void OnTriggerEnter2D(Collider2D other){	
 		if(other.gameObject.tag == "Left"){
 			currentDirection = directions.right;
@@ -51,7 +68,8 @@ public class Enemy_AI : MonoBehaviour
 			currentDirection = directions.left;
 		}
 	}
-	
+
+	// Collisions between enemy's raycast and other tags
 	void rayCollision(RaycastHit2D hit){
 		if(hit.collider != null){
 			if(hit.collider.tag == "Cover"){
