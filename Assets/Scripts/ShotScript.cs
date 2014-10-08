e5418e18619a849c45fcba467c77fdcd1b959b67
TeejStroyer27 <titoriano@gmail.com>
2014-10-08 12:15:30 -0400

    Updated shooting script

--- Assets/Scripts/ShotScript.cs
+++ Assets/Scripts/ShotScript.cs
@@ -2,18 +2,48 @@
 using System.Collections;
 
 public class ShotScript : MonoBehaviour {
-	public float speed = 1.5f;
-	private Vector3 target;
 	
-	void Start () {
-		target = transform.position;
+	public float fireRate=0;
+	public LayerMask whatToHit;
+	float timeToFire=0;
+	Transform firePoint;
+	
+	
+	void Awake() {
+		firePoint=transform.FindChild("FirePoint");
+		if (firePoint==null){
+			Debug.LogError ("No Fire Point!!!");
+		}
+		
 	}
 	
-	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
-			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			target.z = transform.position.z;
+	void Update(){
+		
+		if(fireRate==0){
+			if (Input.GetMouseButtonDown(0)){
+				
+				Shoot();
+			}
 		}
-		transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-	}   
+		else{
+			if (Input.GetMouseButton(0)&& Time.time> timeToFire){
+				
+				timeToFire=Time.time+1/fireRate;
+				Shoot();
+				
+			}
+		}
+	}
+	void Shoot(){
+		Debug.Log("test");
+		Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+		Vector2 firePointPosition = new Vector2(firePoint.position.x,firePoint.position.y);
+		RaycastHit2D hit = Physics2D.Raycast(firePointPosition,mousePosition-firePointPosition, 100, whatToHit);
+		Debug.DrawLine(firePointPosition, (mousePosition-firePointPosition) *100);
+		if (hit.collider != null){
+			Debug.DrawLine(firePointPosition,hit.point,Color.red);
+			Debug.Log("Hit:"+hit.collider.name);
+		}
+	}
+	
 }
