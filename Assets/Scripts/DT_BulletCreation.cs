7669558fdf95f45e7819453c301f30878a428a01
Danny Thorne <dannythorne@gmail.com>
2014-10-10 15:55:10 -0400

    preliminary bullet creation and movement

---
+++ Assets/Scripts/DT_BulletCreation.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DT_BulletCreation : MonoBehaviour {
+	
+	public GameObject BulletPrefab;
+
+	void Update () {
+		
+		if( Input.GetMouseButtonDown(0))
+		{
+			Debug.Log ("Mouse Click");
+			
+			Shoot();
+		}
+	}
+	
+	void Shoot() {
+		
+		GameObject bullet = Instantiate(BulletPrefab) as GameObject;
+
+		bullet.transform.position = transform.position;
+
+		Vector3 clickPoint = Camera.main.ScreenToWorldPoint( Input.mousePosition);
+		clickPoint.z = 0;
+		Vector3 difference = clickPoint - transform.position;
+
+		Debug.Log( difference);
+		Debug.Log( difference.magnitude);
+
+		difference.Normalize (); // Normalizing the direction vector. (Meaning make its length equal to 1.)
+
+		Debug.Log( difference);
+		Debug.Log( difference.magnitude);
+
+		DT_BulletMovement bulletScript = bullet.GetComponent<DT_BulletMovement>();
+		bulletScript.direction = difference;
+		
+		float rotZ = Mathf.Atan2 (difference.y, difference.x) * Mathf.Rad2Deg;	// find the angle in degrees
+		transform.rotation = Quaternion.Euler (0f, 0f, rotZ );
+	}
+}
---
+++ Assets/Scripts/DT_BulletMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DT_BulletMovement : MonoBehaviour {
+
+	private Vector3 pos0; // original position
+	public Vector3 direction; // set by bullet creation script
+	public float speed; // set in GUI
+	public float range; // set in GUI
+
+	// Use this for initialization
+	void Start () {
+
+		pos0 = transform.position;
+
+		Debug.Log( direction);
+		Debug.Log( direction.magnitude);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 pos = transform.position;
+
+		if( Vector3.Distance(pos,pos0) > range)
+		{
+			Destroy( gameObject);
+		}
+
+		//pos.x = pos.x + 0.1f;
+		pos = pos + speed*direction;
+		transform.position = pos;
+	}
+}
