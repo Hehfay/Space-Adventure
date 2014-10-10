5268d19ee71dbb65040efd9be5d6e15d76b06491
TeejStroyer27 <titoriano@gmail.com>
2014-10-09 20:07:18 -0400

    Working on Scaling: Compiles but doesn't work

--- Assets/Scripts/ShotScript.cs
+++ Assets/Scripts/ShotScript.cs
@@ -44,6 +44,8 @@ public class ShotScript : MonoBehaviour {
 		if (hit.collider != null){
 			Debug.DrawLine(firePointPosition,hit.point,Color.red);
 			Debug.Log("Hit:"+hit.collider.name);
+			collider.gameObject.transform.localScale = new Vector3(transform.localScale.x * 1.5f, transform.localScale.y * 1.5f,0f);
+			//collider.gameObject.transform.localScale.y += 0.5f;
 
 		}
 	}
