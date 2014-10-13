f8e3c5faa2278195520649b9369870cbc5cf27e3
TeejStroyer27 <titoriano@gmail.com>
2014-10-12 21:36:09 -0400

    Shrink and Grow gun issues resolved

--- Assets/Scripts/ShotScript.cs
+++ Assets/Scripts/ShotScript.cs
@@ -18,24 +18,45 @@ public class ShotScript : MonoBehaviour {
 	}
 	
 	void Update(){
-		
+		// Shoot Grow
 		if(fireRate==0){
 			if (Input.GetMouseButtonDown(0)){
 				
-				Shoot();
+				ShootGrow();
 			}
 		}
 		else{
 			if (Input.GetMouseButton(0)&& Time.time> timeToFire){
 				
 				timeToFire=Time.time+1/fireRate;
-				Shoot();
+				ShootGrow();
 
 				
 			}
 		}
+		//Shoot Shrink
+
+		if(fireRate==0){
+			if (Input.GetMouseButtonDown(1)){
+				
+				ShootShrink();
+			}
+		}
+		else{
+			if (Input.GetMouseButton(1)&& Time.time> timeToFire){
+				
+				timeToFire=Time.time+1/fireRate;
+				ShootShrink();
+				
+				
+			}
+		}
 	}
-	void Shoot(){
+
+
+	// Grow Function
+
+	void ShootGrow(){
 		Debug.Log("test");
 		Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
 		Vector2 firePointPosition = new Vector2(firePoint.position.x,firePoint.position.y);
@@ -46,12 +67,34 @@ public class ShotScript : MonoBehaviour {
 			Debug.Log("Hit:"+hit.collider.name);
 			//collider.gameObject.transform.localScale = new Vector3(transform.localScale.x * 1.5f, transform.localScale.y * 1.5f,0f);
 			Vector3 s = hit.collider.gameObject.transform.localScale;
-			s.x *= 0.5f;
-			s.y *= 0.5f;
+			s.x *= 1.1f;
+			s.y *= 1.1f;
 			hit.collider.gameObject.transform.localScale = s;
 
 			//collider.gameObject.transform.localScale.y += 0.5f;
 
+		}
+	}
+
+	// Shrink Function
+
+	void ShootShrink(){
+		Debug.Log("test");
+		Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+		Vector2 firePointPosition = new Vector2(firePoint.position.x,firePoint.position.y);
+		RaycastHit2D hit = Physics2D.Raycast(firePointPosition,mousePosition-firePointPosition, 100, whatToHit);
+		Debug.DrawLine(firePointPosition, (mousePosition-firePointPosition) *100);
+		if (hit.collider != null){
+			Debug.DrawLine(firePointPosition,hit.point,Color.green);
+			Debug.Log("Hit:"+hit.collider.name);
+			//collider.gameObject.transform.localScale = new Vector3(transform.localScale.x * 1.5f, transform.localScale.y * 1.5f,0f);
+			Vector3 s = hit.collider.gameObject.transform.localScale;
+			s.x *= .9f;
+			s.y *= .9f;
+			hit.collider.gameObject.transform.localScale = s;
+			
+			//collider.gameObject.transform.localScale.y += 0.5f;
+			
 		}
 	}
 	
