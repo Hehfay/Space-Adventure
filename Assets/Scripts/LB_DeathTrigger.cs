00d4b0f70bb8090e5ffc69801e0497fbf8462f8b
laura-cs <lbuckma0@georgetowncollege.edu>
2014-10-29 12:15:34 -0400

    Script for Death Trigger
    Script for death trigger when Player collides with Death Zone ground
    object.

---
+++ Assets/Scripts/LB_DeathTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LB_DeathTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.collider2D.tag == "Player") {
+			Debug.Log("You are dead.");
+
+				}
+	}
+}
