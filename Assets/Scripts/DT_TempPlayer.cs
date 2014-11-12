0e966197476e77b50f1a56974cf3b1826c0a3384
Danny Thorne <dannythorne@gmail.com>
2014-11-12 15:20:46 -0500

    player stays on scene change

---
+++ Assets/Scripts/DT_TempPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DT_TempPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	  DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.x = pos.x + 0.1f;
+		transform.position = pos;
+	}
+
+}
