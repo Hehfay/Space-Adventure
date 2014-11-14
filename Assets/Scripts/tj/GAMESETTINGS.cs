086d33a5f3769651295f8f8ee4acd720f6362052
TeejStroyer27 <titoriano@gmail.com>
2014-11-14 01:48:47 -0500

    MinorImprovements

--- Assets/Scripts/tj/GAMESETTINGS.cs
+++ Assets/Scripts/tj/GAMESETTINGS.cs
@@ -5,6 +5,9 @@ public class GAMESETTINGS : MonoBehaviour {
 	
 	public static string PreviousDoor;
 	public static string CurrentScene;
+	public static int PLAYERHEALTH;
+	public static int CURRENTWEAPON;
+	public static string ACTIVEGUNTEXT;
 	// Use this for initialization
 	
 	
@@ -23,6 +26,10 @@ public class GAMESETTINGS : MonoBehaviour {
 		DontDestroyOnLoad(this);
 			  PreviousDoor = "none";
 			  CurrentScene ="00_00StartScene";
+			  PLAYERHEALTH = 10;
+			  CURRENTWEAPON=1;
+			  ACTIVEGUNTEXT="Grow and Shrink";
+			  
 
 	}
 }
--- Assets/Scripts/tj/WeaponSelect.cs
+++ Assets/Scripts/tj/WeaponSelect.cs
@@ -17,11 +17,17 @@ public class WeaponSelect : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.Alpha1)){ //if I press the "1" button, switch currentWeapon to 1.
 			currentWeapon = 1;
 			ActiveGunText.text = "Grow and Shrink";
+			GAMESETTINGS.ACTIVEGUNTEXT="Grow and Shrink";
 		}
 		else if(Input.GetKeyDown(KeyCode.Alpha2)){ //if I press the "1" button, switch currentWeapon to 2.
 			currentWeapon = 2;
 			ActiveGunText.text = "Telekinesis";
+			GAMESETTINGS.ACTIVEGUNTEXT="Telekinesis";
 		}
 	
+	}
+	void Awake(){
+		GAMESETTINGS.CURRENTWEAPON = currentWeapon;
+		ActiveGunText.text = GAMESETTINGS.ACTIVEGUNTEXT;
 	}
 }
