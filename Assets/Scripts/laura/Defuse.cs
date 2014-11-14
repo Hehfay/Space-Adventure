099b33d3a60ab6b436e8f70aa6baff47058606b0
laura-cs <lbuckma0@georgetowncollege.edu>
2014-11-14 17:29:14 -0500

    Defuse Script

---
+++ Assets/Scripts/laura/Defuse.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Defuse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.collider2D.tag == "Player") {
+		Debug.Log("Congratulations! You defused the bomb in time!");
+		//Application.LoadLevel();
+		}
+	}
+	}
+}
